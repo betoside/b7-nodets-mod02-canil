d815f1b7eb9976badbfaf4642c89a44a57483422
Betoside <betoside@gmail.com>
2023-07-31 21:49:11 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # b7-nodets-mod02-canil
+Projeto treinamento Node + Typescript
+
+### Pré-requisitos globais
+`npm i -g nodemon typescript ts-node`
+
+### Instalação
+`npm install`
+
+### Para rodar o projeto
+`npm run startdev`
---
+++ src/controllers/pageController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+
+export const home = (req: Request, res: Response) => {
+
+    res.send('Home no controller');
+
+    // res.render('pages/page');
+
+};
+
+export const dogs = (req: Request, res: Response) => {
+
+    res.send('dogs no controller');
+    // res.render('pages/page');
+
+};
+
+export const cats = (req: Request, res: Response) => {
+
+    res.send('cats no controller');
+    // res.render('pages/page');
+
+};
+
+export const fishes = (req: Request, res: Response) => {
+
+    res.send('fishes no controller');
+    // res.render('pages/page');
+
+};
